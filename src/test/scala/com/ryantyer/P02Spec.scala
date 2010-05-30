2937b0e726831d82c90b1fe13dd3df0c746d1fbb
Ryan Tyer <ryan.tyer@gmail.com>
2010-05-30 11:12:35 -0600

    finished solution

--- src/test/scala/com/ryantyer/P02Spec.scala
+++ src/test/scala/com/ryantyer/P02Spec.scala
@@ -6,9 +6,28 @@ class P02Spec() extends Spec {
 	describe("an imperative solution"){
 		it("should return 4 from List(1,2,4,8)"){
 			expect(4){
-				pending
+				P02.imperativeSecondToLast(List(1,2,4,8))
+			}					
+		}
+		it("should return dick from List(tom,dick,jane)"){
+			expect("dick"){
+				P02.imperativeSecondToLast(List("tom", "dick", "jane"))
 			}
 		}
 	}
 	
+	describe("a functinonal solution"){
+		it("should return 4 from List(1,2,3,8)"){
+			expect(4){
+				P02.functionalSecondToLast(List(1,2,4,8))
+			}
+		}
+	}	
+	describe("a functinonal solution with matching"){
+		it("should return 4 from List(1,2,3,8)"){
+			expect(4){
+				P02.functionalMatchingSecondToLast(List(1,2,4,8))
+			}
+		}
+	}
 }
