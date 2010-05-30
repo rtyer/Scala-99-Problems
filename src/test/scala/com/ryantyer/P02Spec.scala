4c2c768013f1a4ac964aba954e450eb212c4302c
Ryan Tyer <ryan.tyer@gmail.com>
2010-05-30 01:10:49 -0600

    built spec for imperative

---
+++ src/test/scala/com/ryantyer/P02Spec.scala
@@ -0,0 +1,14 @@
+package com.ryantyer
+
+import org.scalatest.Spec
+
+class P02Spec() extends Spec {
+	describe("an imperative solution"){
+		it("should return 4 from List(1,2,4,8)"){
+			expect(4){
+				pending
+			}
+		}
+	}
+	
+}
